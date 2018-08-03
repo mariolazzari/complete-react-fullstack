afebcca0f41a7c01656f7dd2db9bc66e891b926c
mariolazzari <mario.lazzari@gmail.com>
2018-08-03 23:08:40 +0200

    Done lesson 19

--- 01_react_basics/src/components/header.js
+++ 01_react_basics/src/components/header.js
@@ -3,12 +3,10 @@ import "../css/styles.css";
 
 class Header extends Component {
   state = {
-    title: "The keywords are:",
     keywords: ""
   };
 
   inputChangeHandler = event => {
-    //console.log("changed", event.target.value);
     this.setState({ keywords: event.target.value });
   };
 
@@ -17,7 +15,6 @@ class Header extends Component {
       <header>
         <div className="logo">Logo</div>
         <input type="text" onChange={this.inputChangeHandler} />
-        <div>{this.state.title}</div>
       </header>
     );
   }
--- 01_react_basics/src/index.js
+++ 01_react_basics/src/index.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/header";
+import NewsList from "./components/news_list";
+import JSON from "./db.json";
 
-const App = () => {
-  return (
-    <div>
-      <Header />
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    news: JSON
+  };
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <NewsList news={this.state.news}>
+          <h3>The news are:</h3>
+        </NewsList>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
